7ee3eca55ec1a3c0bdaad2b3e09afb8b4a6bd193
Lee Morgan <MorganLee909@gmail.com>
2018-02-22 04:05:33 -0500

    Trimmed down php/html to just the buttons I need.  Changed php to call a method from python.

--- index.php
+++ index.php
@@ -6,36 +6,23 @@
 </head>
 
   <?php
-    //TODO: The shell execution needs to be removed from every if statement and
-    //added to the end.  Also I want to change this to call a method.
+    $pin = 0;
     if(isset($_POST['first'])){
       $pin = 2;
-      shell_exec("sudo python relayControl.py " .$pin);
     }
     else if(isset($_POST['second'])){
       $pin = 3;
-      shell_exec("sudo python relayControl.py" .$pin);
     }
+
+    shell_exec("python -c 'from relayControl import switchOnOff; switchOnOff()'" .$pin)
   ?>
 
 <body>
-  <h4><span id="datetime"></span></h4>
-  <!-- TODO: Change date/time code to be less ugly and make it dynamic -->
-  <script>
-    var dt = new Date();
-    document.getElementById("datetime").innerHTML = (("0"+dt.getDate()).slice(-2)) +
-    "."+ (("0"+(dt.getMonth()+1)).slice(-2)) +"."+ (dt.getFullYear()) +" "+
-    (("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2)) +":"+
-    (("0"+dt.getSeconds()).slice(-2));
-  </script>
   <h1>Lee Morgan</h1>
   <div>
     <form method="post">
-      <button name="Light">First</button>
-      <button name="Fan">Second</button>
+      <button name="first">Light</button>
+      <button name="second">Fan</button>
   </div>
-  <script>
-    document.body.style.backgroundColor = "#373738";
-  </script>
 </body>
 </html>
--- relayControl.py
+++ relayControl.py
@@ -11,15 +11,16 @@ for i in pinList:
     GPIO.setup(i, GPIO.OUT)
 
 #Get the pin number passed by PHP and cast to int
-pinRaw = sys.argv[1]
-pin = int(pinRaw)
-
-#TODO: turn this into a method and call method from PHP
 #determine whether the relay is on or off and switch it
-if(GPIO.input(pin) == 0):
-    GPIO.output(pin, GPIO.HIGH)
-elif(GPIO.input(pin) == 1):
-    GPIO.output(pin, GPIO.LOW)
+def switchOnOff():
+    #Get pin input from terminal and cast to int
+    pinRaw = sys.argv[1]
+    pin = int(pinRaw)
+
+    if(GPIO.input(pin) == 0):
+        GPIO.output(pin, GPIO.HIGH)
+    elif(GPIO.input(pin) == 1):
+        GPIO.output(pin, GPIO.LOW)
 
 #return the state of the particular pin to determine if it is on or off
 def getState(pin):
