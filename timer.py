bb9181601d3730b356287c6d851f52c418e99eef
Lee <MorganLee909@gmail.com>
2018-03-07 01:19:09 -0500

    Added double motion sensor.  Needs checking

--- timer.py
+++ timer.py
@@ -2,17 +2,42 @@
 from datetime import datetime
 import time
 import relayControl
+import RPi.GPIO as GPIO
+
+#Set up the GPIO and the pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(11, GPIO.IN)
+GPIO.setup(5, GPIO.IN)
+inSensor = GPIO.input(11)
+outSensor = GPIO.input(5)
+
+occupancy = 0
+dayOfWeek = ""
+currentTime = ""
 
 def afterWork():
     if (dayOfWeek == "Tue" or "Wed" or "Thu" or "Fri" or "Sat"):
         if (currentTime == "0450"):
             relayControl.recieveInput(8)
 
-dayOfWeek = ""
-currentTime = ""
+def motionCheck():
+    if(inSensor == 1):
+        time.sleep(0.1)
+        if(outSensor ==1):
+            occupancy -= 1
+            if(occupancy == 0):
+                relayControl.recieveInput(8)
+    if(outSensor == 1):
+        time.sleep(0.1)
+        if(inSensor == 1):
+            if(occupancy == 0)
+                relayControl.recieveInput(8)
+            occupancy += 1
 
 while True:
     dayOfWeek = datetime.now().strftime("%a")
     currentTime = datetime.now().strftime("%H%M")
     afterWork()
-    time.sleep(59)
+    motionCheck()
+    time.sleep(0.1)
